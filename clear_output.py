0219a55b23c2f4f0a374326e76da07e6ff3509b1
range-vs <trubnicov_ivan1996@mail.ru>
2024-06-08 20:35:46 +0300

    + fix script clear output folders

--- clear_output.py
+++ clear_output.py
@@ -1,12 +1,8 @@
 import os
 import shutil
 
-root_folder = "output"
 bin_dir = "bin"
 obj_dir = "obj"
-
-if os.path.exists(root_folder):
-    shutil.rmtree(root_folder)
 
 for root, dirs, files in os.walk("."):
     for dir in dirs:
